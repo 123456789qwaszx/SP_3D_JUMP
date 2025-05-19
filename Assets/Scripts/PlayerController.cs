0ad16d235bd5a1652d56425da7752e38acf2a890
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-05-19 18:00:33 +0900

    Remove :  Inputsystem Move삭제
    인풋시스템에서 wasd로 Vector2값을 받는 건, 디버깅없이는 이해가 불가능하고,
    기능도 제한되는 것 같아서 삭제함

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
+
+    public float _moveSpeed;
+    public float jumpPower;
+
+    private Vector2 InputsystemInput;
+
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        // 이미 인풋시스템에서 w시 transformposition을 Vector2.(x, y) 값이 1씩 들어가게 세팅해둠.
+        if (context.phase == InputActionPhase.Performed)
+        {
+
+
+        }
     }
 
-    void Update()
-    {
-        
-    }
+
 }
