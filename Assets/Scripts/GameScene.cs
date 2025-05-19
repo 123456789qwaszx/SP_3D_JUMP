910b9454e8797a0e25f50b0cbe7240b399512be7
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-05-19 16:33:25 +0900

    Reapply "Add : Manager 추가"
    This reverts commit 321d805861885a5ef0fafa780c604cf414ad04bd.

---
+++ Assets/Scripts/GameScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : BaseScene
+{
+    protected override void Init()
+    {
+        base.Init();
+        
+    }
+
+    void Start()
+    {
+        
+    }
+      
+    public override void Clear()
+    {
+        
+    }
+}
