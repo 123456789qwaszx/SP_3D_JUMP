3d734c0d850dba9e9b0f047bc4d10691fae4e3c1
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-05-20 10:32:58 +0900

    Add : 캐릭터 달리는 애니, Idel애니 작성
    캐릭터 달리는 애니, Idel애니 프레임 작성

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,18 +10,37 @@ public class PlayerController : MonoBehaviour
     public float _moveSpeed;
     public float jumpPower;
     public Rigidbody _rigidbody;
+    private Animator animator;
 
     void Start()
     {
         // 이벤트가 두번 호출되는 것 방지
-        Managers.Char.KeyAction -= OnKeyboard;
-        Managers.Char.KeyAction += OnKeyboard;
-        _rigidbody = GetComponent<Rigidbody>();
+        Managers.Char.KeyAction -= KeyboardMove;
+        Managers.Char.KeyAction += KeyboardMove;
+        
+        Managers.Char.KeyAction -= KeyboardAnim;
+        Managers.Char.KeyAction += KeyboardAnim;
 
+        _rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
 
-    public void OnKeyboard()
+    // 좌표이동x 애니메이션만 실행. 아마 벽에 부딪혔을 때에도 실행이 되면 좋을 것 같아.
+    public void KeyboardAnim()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.Play("RUN");
+        }
+        else
+        {
+            animator.Play("WAIT");
+        }
+    }// -> 이렇게 하나하나 구현하기보다는 KeyboardMove 중일 때 특정애니메이션 실행. 이렇게 묶는게 좋을 듯. 어차피 바꿀거니 W만 만들어서 테스트해보자.
+
+    // 실제로 좌표가 이동
+    public void KeyboardMove()
     {
         if (Input.GetKey(KeyCode.W))
         {
